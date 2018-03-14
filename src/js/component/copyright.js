a3a24eb48e5f5bda64014e47f338fdb5d8904c3c
戴江涛 <captaininphw@gmail.com>
2018-03-14 13:44:02 +0800

    docs: :memo: add VUE global component: copytight.js

---
+++ src/js/component/copyright.js
@@ -0,0 +1,26 @@
+/*
+ * copyright.js
+ * Copyright (C) 2018 daijt
+ *
+ * Distributed under terms of the MIT license.
+ */
+(function () {
+    'use strict';
+
+    {
+        Vue.component('copyright', {
+            template: `
+            <footer class="bg-light text-center">
+                <p class="declaration text-dark font-italic font-weight-light">
+                    <span class="text-primary font-weight-bold">Vue&nbsp;Resume&nbsp;Editor&nbsp;</span>by&nbsp;戴江涛
+                </p>
+                <p class="copyright font-weight-light text-gray">
+                    <a href="https://github.com/CaptainInPHW/vue-online-resume-editor">github.com/CaptainInPHW.</a>&nbsp;All&nbsp;Rights&nbsp;Reserved.<br>
+                    <span>©&nbsp;CopyRight&nbsp;2018-2019</span>
+                </p>
+            </footer>
+           `
+        })
+    }
+
+})();
