b7b6b6345d02bbce5497e00314333b6804bc9d0d
戴江涛 <captaininphw@gmail.com>
2018-03-14 13:48:24 +0800

    docs: :memo: add vue-router config: router.js

---
+++ src/js/route/router.js
@@ -0,0 +1,39 @@
+/*
+ * router.js
+ * Copyright (C) 2018 daijt
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+import Edit from './page-edit';
+import SignUp from './page-signup';
+import SignIn from './page-signin';
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '*',
+            redirect: '/edit'
+        },
+        {
+            path: '/',
+            redirect: '/edit'
+        },
+        {
+            path: '/edit',
+            props: true,
+            component: Edit
+        },
+        {
+            path: '/signup',
+            props: true,
+            component: SignUp
+        },
+        {
+            path: '/signin',
+            props: true,
+            component: SignIn
+        },
+    ]
+});
